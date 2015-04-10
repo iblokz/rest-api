5b8b80c1bc1c07ca782efebfb65eaf85eaf1e5f9
Alex Milanov <alex@i4web.biz>
2015-04-10 23:34:52 +0300

    parser initial validator support, crudify restruturing & validation, example

--- example/validlog/server.js
+++ example/validlog/server.js
@@ -14,7 +14,7 @@ var app = express();
 
 //mongoose.connect('mongodb://localhost/express_test'); // connect to our database
 
-var db = mongoose.connect('mongodb://localhost/restify_example');
+var db = mongoose.connect('mongodb://localhost/restify_validlog');
 
 app.set('port',process.env.PORT || 3000);
 //app.set('views',__dirname+'/app/views');
@@ -26,13 +26,11 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-app.use(express.static(__dirname+"/public"));
+//app.use(express.static(__dirname+"/public"));
 
 app.get("/", function(req,res){
 	res.send("Hello World!");
 });
-
-
 
 
 // init model
@@ -48,7 +46,7 @@ var usersCrud = restify.crudify('User',db);
 var articlesCrud = restify.crudify('Article',db);
 
 // set up example data
-require('./dbSetup')(db);
+//require('./dbSetup')(db);
 
 customCtrls.users.me = function(req, res){
 
@@ -68,46 +66,8 @@ customCtrls.users.me = function(req, res){
 	
 }
 
-customCtrls.users.articles = function(req, res){
-
-	var userStore = req.store;
-
-	articlesCrud.promiseList({ createdBy: userStore._id }).then(
-		function(list){
-			res.jsonp(list);
-		},function(err){
-			res.jsonp({error: err});
-		});
-}
-
-
 
 restify.initRoutes(app,restMap,customCtrls,db);
-
-
-/*
-app.get(/\/users\/?(\d*)?\/?/, function(req, res){
-
-	var title = "";
-
-	// todo handle different response types: html, json
-
-	// get ...
-	if(req.params && req.params[0]){
-		// single user
-		title = 'Get single user';
-	} else {
-		// users
-		title = 'Get users';
-
-	}
-
-	res.render("users", {title: title});
-});
-*/
-
-
-// app.get("/:collection/:controller:id/:controller")
 
 
 http.createServer(app).listen(app.get('port'),function(){
