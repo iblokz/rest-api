759f1f8061b2b37b14746ab2bf13c11e5d1d1534
Alex Milanov <alex@i4web.biz>
2015-07-13 23:12:42 +0300

    updated example

--- example/full/dbSetup.js
+++ example/full/dbSetup.js
@@ -1,43 +1,67 @@
-"use strict"
+"use strict";
 
-	var crudify = require("../lib/crudify.js");
+/**
+ * First we set the node enviornment variable if not set before
+ */
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-	module.exports = function(db){
+/**
+ * Module dependencies.
+ */
+var config = require('./config/config');
+var mongoose = require('mongoose');
+var chalk = require('chalk');
 
-		var usersCrud = crudify('User',db);
-		var articlesCrud = crudify('Article',db);
+/**
+ * Main application entry file.
+ * Please note that the order of loading is important.
+ */
+// Bootstrap db connection
+var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
+	if (err) {
+		console.error(chalk.red('Could not connect to MongoDB!'));
+		console.log(chalk.red(err));
+	}
+});
 
-		var userDocument = {
-			"name" : "John Doe",
-			"email" : "test@test.com",
-			"password" : "secret"
+
+// restify
+var restify = require("../../lib/restify.js");
+var restMap = require("./data/rest.json");
+
+var crudify = restify.crudify;
+
+require('./app/models/user');
+restify.loadModel(restMap, db);
+
+var usersCrud = crudify('User',db);
+var articlesCrud = crudify('Article',db);
+
+var userDocument = {
+	"name" : "John Doe",
+	"email" : "test@test.com",
+	"password" : "secret"
+};
+
+var articleDocument = {
+	"title" : "Test Article",
+	"body" : "Test Test"
+}
+
+
+usersCrud.promiseCreate(userDocument).then(function(userStore){
+
+	articleDocument.createdBy = userStore._id;
+
+	return articlesCrud.promiseCreate(articleDocument).then(function(articleStore){
+		return {
+			msg: 'Succesfuly imported example data',
+			user: userStore,
+			article: articleStore
 		};
-
-		var articleDocument = {
-			"title" : "Test Article",
-			"body" : "Test Test"
-		}
-
-		usersCrud.promiseList().then(function(usersList){
-			// if users empty create test data
-			if(usersList.total == 0){
-				usersCrud.promiseCreate(userDocument).then(function(userStore){
-
-					articleDocument.createdBy = userStore._id;
-
-					return articlesCrud.promiseCreate(articleDocument).then(function(articleStore){
-						return {
-							msg: 'Succesfuly imported example data',
-							user: userStore,
-							article: articleStore
-						};
-					});
-				}).then(function(result){
-					console.log(result);
-				},function(errors){
-					console.log(errors);
-				})
-			}
-		});
-
-	}	
+	});
+}).then(function(result){
+	console.log(result);
+},function(errors){
+	console.log(errors);
+})
