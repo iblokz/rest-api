d9761a8bfafbdeba4a8ae2ae7d25aca17d018394
Alex Milanov <alex@i4web.biz>
2017-03-03 00:26:59 +0200

    replaced github repo dependency with npm package, some fixes

--- example/full/app/models/user.js
+++ example/full/app/models/user.js
@@ -1,23 +1,19 @@
-'use strict'
+'use strict';
 
-var mongoose = require('mongoose');
-var crypto   = require('crypto');
-
-
-
+const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 // define the schema for our user model
 // role - admin, user
 var userSchema = mongoose.Schema({
-	name : String,
-	role : { type: String, default: 'user' },
-	email : String,
-	password : String,
-	salt : String,
-	registerDate : { type: Date, default: Date.now },
-	lastLoginDate : Date
+	name: String,
+	role: {type: String, default: 'user'},
+	email: String,
+	password: String,
+	salt: String,
+	registerDate: {type: Date, default: Date.now},
+	lastLoginDate: Date
 });
-
 
 /**
  * Hook a pre save method to hash the password
@@ -33,13 +29,10 @@ userSchema.pre('save', function(next) {
 /**
  * Create instance method for hashing a password
  */
-userSchema.methods.hashPassword = function(password) {
-	if (this.salt && password) {
-		return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
-	} else {
-		return password;
-	}
-};
+userSchema.methods.hashPassword = password =>
+	(this.salt && password)
+		? crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64')
+		: password;
 
 /**
  * Create instance method for authenticating user
@@ -47,7 +40,6 @@ userSchema.methods.hashPassword = function(password) {
 userSchema.methods.authenticate = function(password) {
 	return this.password === this.hashPassword(password);
 };
-
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
